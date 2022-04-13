25f6e28a2a1e3431bcf9bda90702a26b083ed5d5
amitay <service@smart-e.co.il>
2022-04-13 12:20:24 +0300

    origin initial commit

---
+++ forms.py
@@ -0,0 +1,66 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField, HiddenField
+from wtforms.validators import DataRequired, URL, Email
+from flask_ckeditor import CKEditorField
+from flask_login import UserMixin
+
+from urlparse2 import urlparse2
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+
+def get_redirect_target():
+    for target in request.args.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
+
+
+class RedirectForm(FlaskForm):
+    next = HiddenField()
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        if not self.next.data:
+            self.next.data = get_redirect_target() or ''
+
+    def redirect(self, endpoint='home', **values):
+        if is_safe_url(self.next.data):
+            return redirect(self.next.data)
+        target = get_redirect_target()
+        return redirect(target or url_for(endpoint, **values))
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+	title = StringField("Blog Post Title", validators=[DataRequired("Please enter title")])
+	subtitle = StringField("Subtitle", validators=[DataRequired("Please enter subtitle")])
+	img_url = StringField("Blog Image URL", validators=[DataRequired("Please enter url"), URL("Please enter a valid url")])
+	body = CKEditorField("Blog Content", validators=[DataRequired("Please write somthing")])
+	submit = SubmitField("Submit Post")
+
+
+class NewUser(UserMixin, RedirectForm):
+	name = StringField("Name:*", validators=[DataRequired("Please enter your name")])
+	email = EmailField("Email:*", validators=[DataRequired("Please enter your email"),
+	                                         Email("Please enter a valid email address")])
+	phone = StringField("Phone:")
+	password = PasswordField("Password:*", validators=[DataRequired("Please enter your password")])
+	submit = SubmitField("Register")
+
+
+class Login(RedirectForm):
+	email = EmailField("Email:", validators=[DataRequired("Please enter your email"),
+	                                         Email("Please enter a valid email address")])
+	password = PasswordField("Password:", validators=[DataRequired("Please enter your password")])
+	submit = SubmitField("Register")
+
+class CommentForm(FlaskForm):
+	body = CKEditorField("Comment", validators=[DataRequired("Please write somthing")])
+	submit = SubmitField("Submit Comment")
---
+++ main.py
@@ -0,0 +1,244 @@
+import requests as rq
+import os
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from time import sleep
+from datetime import datetime, timedelta
+from dateutil import parser
+from forms import CreatePostForm, NewUser, Login, CommentForm
+from flask_gravatar import Gravatar
+
+# --- CREATE and CONFING Flask APP
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+#CONNECT and CONFING DB
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///helium-israel.db").replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+## CONNECTING BOOTSTRAP5 TO FLASK
+bootstrap = Bootstrap5(app)
+
+## CONNECTING AND CONFIGURING GRAVATAR
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
+## ESTABLISH USER SESSIONS MANAGMENT
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+## Construct miners data base
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.Text)
+    email = db.Column(db.String(70), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    wallets = relationship("Wallet", back_populates="user")
+
+    def __repr__(self):
+	    return f"User ID: {self.id}"
+
+# db.create_all()
+
+
+class Wallet(db.Model):
+	__tablename__ = "wallets"
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	address = db.Column(db.Text, unique=True, nullable=False)
+
+	miners = relationship("Miner", back_populates="wallet")
+
+	user_id = db.Column(db.Integer, ForeignKey("users.id"))
+	user = relationship("User", back_populates="wallets")
+
+	def __repr__(self):
+		return f"Wallet: {self.address}"
+
+# db.create_all()
+
+
+class Miner(db.Model):
+	__tablename__ = "miners"
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	name = db.Column(db.String(100), unique=True, nullable=False)
+	address = db.Column(db.Text, nullable=False)
+	added = db.Column(db.DateTime, nullable=False)
+	city = db.Column(db.Text)
+	country = db.Column(db.Text)
+	street = db.Column(db.Text)
+	online = db.Column(db.String(20))
+	earnings_7 = db.Column(db.Float)
+	earnings_30 = db.Column(db.Float)
+
+	wallet_address = db.Column(db.Integer, ForeignKey("wallets.address"))
+	wallet = relationship("Wallet", back_populates="miners")
+
+	def __repr__(self):
+		return f"name: {self.name} - City: {self.city} - earining 30 days: {self.earnings_30}"
+
+
+# Line below only required once, when creating DB.
+# db.create_all()
+
+def get_miners_data():
+	headers = {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+	}
+
+	# Get all hotspots in Israel
+	israel_box = {
+		"cursor": None,
+		"swlat": 29.4255538961337,
+		"swlon": 34.46116814079567,
+		"nelat": 33.03202086923252,
+		"nelon": 35.613699832312264,
+	}
+
+	url = "https://api.helium.io/v1/hotspots/location/box"
+	# url = "https://api.helium.io/v1/hotspots/11sP9aF7QWkA5hYnnFkcyKt5wxkfW4UqaesG5WD8fzirpawB2v9"
+	response = rq.get(url, headers=headers, params=israel_box)
+	response.raise_for_status()
+	data = response.json()
+
+	miners = data["data"]
+	# print(miners)
+
+	time = datetime.now()
+	last_7_days = time - timedelta(days=7)
+	last_30_days = time - timedelta(days=30)
+
+	count = 0
+	for m in miners:
+		sleep(1)
+		count += 1
+		print(f"{count} - {m['name']}")
+
+		url = f"https://api.helium.io/v1/hotspots/{m['address']}/rewards/sum"
+		parameters = {
+			"max_time": time.isoformat(),
+			"min_time": last_7_days.isoformat(),
+		}
+		response = rq.get(url, headers=headers, params=parameters)
+		response.raise_for_status()
+		data_7 = response.json()
+		earining_7 = int(data_7["data"]['sum']) / 100000000
+		print(earining_7)
+		sleep(1)
+
+		url = f"https://api.helium.io/v1/hotspots/{m['address']}/rewards/sum"
+		parameters = {
+			"max_time": time.isoformat(),
+			"min_time": last_30_days.isoformat(),
+		}
+		response = rq.get(url, headers=headers, params=parameters)
+		response.raise_for_status()
+		data_30 = response.json()
+		earining_30 = int(data_30["data"]['sum']) / 100000000
+		print(earining_30)
+		sleep(1)
+
+		if Miner.query.filter_by(name=m['name']).first() == None:
+			miner = Miner(
+				name=m['name'],
+				address=m['address'],
+				added=parser.parse(m['timestamp_added']),
+				city=m['geocode']['long_city'],
+				country=m['geocode']['long_country'],
+				street=m['geocode']['long_street'],
+				online=m['status']['online'],
+				earnings_7=earining_7,
+				earnings_30=earining_30,
+				wallet_address=m["owner"],
+			)
+			db.session.add(miner)
+
+			print(miner)
+			print(f"{m['name']} - whole record was added to db\n-------------------------------------------------->")
+		else:
+			# break  #USE this break to only update the db with new miners
+			miner = Miner(
+				city=m['geocode']['long_city'],
+				country=m['geocode']['long_country'],
+				street=m['geocode']['long_street'],
+				online=m['status']['online'],
+				earnings_7=earining_7,
+				earnings_30=earining_30,
+			)
+
+			print(miner)
+			print("db recored udpated\n-------------------------------------------------->")
+
+		if Wallet.query.filter_by(address=m["owner"]).first() == None:
+			wallet = Wallet(
+				address=m["owner"],
+			)
+			db.session.add(wallet)
+		db.session.commit()
+
+
+# get_miners_data()
+
+
+## Construct APP structure
+@app.route("/")
+def home():
+	response = rq.get("https://api.helium.io/v1/oracle/prices/current")
+	response = response.json()
+	price = str(response["data"]["price"])
+	hnt = f"${price[:2]}.{price[2:4]}"
+	print(hnt)
+
+	def other_miners(wallet_address):
+		wallet = Wallet.query.filter_by(address=wallet_address).first()
+		# print(wallet)
+		miners_count = len(wallet.miners)
+		# print(miners_count)
+		return miners_count
+
+	miners = Miner.query.filter_by(online="online").all()
+
+	return render_template("index.html", oracle_price=hnt, miners=miners, other_miners=other_miners)
+
+
+@app.route("/wallet/<address>")
+def wallet(address):
+	wallet = Wallet.query.filter_by(address=address).first()
+	miners = wallet.miners
+	print(miners)
+	# .order_by(Miner.added.desc()).limit(10).all()
+
+	return render_template("wallet.html", miners=miners, wallet=wallet)
+
+
+@app.route("/test")
+def test():
+	return render_template("test.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=False)
